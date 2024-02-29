e05af7057878cd9af8febeb428f82e99f9a0f60b
gangwolfmail <129858122+gangwolfmail@users.noreply.github.com>
2024-02-29 11:45:21 +0000

    Deploying to gh-pages from @ gangwolfmail/JS-11@18664451c4c3ffe937d3f7fd01f14fa5f0eb1d52 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,9 +1,10 @@
 import{b as m}from"./assets/vendor-9b35bc79.js";(function(){const a=document.createElement("link").relList;if(a&&a.supports&&a.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))l(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const o of t.addedNodes)o.tagName==="LINK"&&o.rel==="modulepreload"&&l(o)}).observe(document,{childList:!0,subtree:!0});function n(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function l(e){if(e.ep)return;e.ep=!0;const t=n(e);fetch(e.href,t)}})();const r="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACMAAAAjCAYAAAAe2bNZAAAACXBIWXMAABYlAAAWJQFJUiTwAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAGKSURBVHgBzdgxVoNAEAbgIdsgFXACvYGlpTdRu5SWdnqD2FqJN9AbeAUvAnaUOLMv5GHC7s7szr6X/z3ekiwwX1gC2QCcY+q6fm6aZqAF13eQOWv1zL5jVxTFE66WtOD6TVVVl+M4fkGGtG37js3jsl5ZYkOdpCPT8U64Udf3/QMohiDTNN2vdP1ufDvSTvtPkRtiY4cJh+QCm1vHNtcaQxaCYN+bxWChbyx4RYUhAygEwcvhYxiGrZnfwEKfOUAcCF6Xtt8sO7RBEsgJRhMkhaxiNEAxECcmBRQL8WJiQCkQ2w+MYJEOi9x5inTUpkDYGA7IW4QBoRhghjFkSRARJgYkgYgxEpAUQtlAXCalbf5FfGZCX99FxM8yEUYAiQKxMREQMYiF4dxZcfmBxIdrEMO9xWs87Y0GZH6dCjJaEA2Q0YSkgow2JAVkckBiQSYXJAZ0mGtjswVliAQUnGtrQJYJ/EDzz7U1IRQ6Fh3T1e+ca2tD5riGjObahxd4Cl/nP29w/QUyZ63eHxt6jFIw0uczAAAAAElFTkSuQmCC",d=document.querySelector(".btn-footer");function A(){document.body.style.overflow="hidden"}function p(){document.body.style.overflow="visible"}const i=m.create('<div class="modal-team"></div>',{onShow:()=>{document.addEventListener("keydown",c),A()},onClose:()=>{document.removeEventListener("keydown",c),p()}});function u(){i.show();const s=document.querySelector(".modal-team");s.innerHTML=`<div class="modal modal-our-team container">
     <section class="our-team">
       <button class="modal-close-btn" type="button" aria-label="close">
+      
         <img class="modal-close-svg" src="${r}" alt="" width="24" height="24" loading="lazy"/>
       </button>
-       <h2 class = "title-team">Team <span class = "title-shark">JS 11</span></h2>
+       <h2 class = "title-team">Team <span class = "title-11">JS 11</span></h2>
       <ul class="list team-list">
       <li class = "team-item">
       <h3 class = "team-name">FirstName LastName</h3>
@@ -46,5 +47,5 @@ import{b as m}from"./assets/vendor-9b35bc79.js";(function(){const a=document.cre
       </ul>
     </section>
     </div>
-    `,document.querySelector(".modal-close-btn").addEventListener("click",h)}function h(s){i.close()}function c(s){s.code==="Escape"&&i.close()}d.addEventListener("click",u);
+    `,document.querySelector(".modal-close-btn").addEventListener("click",f)}function f(s){i.close()}function c(s){s.code==="Escape"&&i.close()}d.addEventListener("click",u);
 //# sourceMappingURL=commonHelpers.js.map
